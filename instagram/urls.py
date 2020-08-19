253c53a3271b0f04ca7f7ba4e2bc51b4243fcc69
nelleeeee <rudghksldl@gmail.com>
2020-08-19 23:05:41 +0900

    drf 기본구조 구현

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,1 +1,11 @@
-urlpatterns = []
+from django.urls import include, path
+from rest_framework import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register("post", views.PostViewSet)
+# router.urls <- url pattern list
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
